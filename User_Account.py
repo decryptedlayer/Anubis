163898d7017f42e253befc6e11a8d5bb3b4479d0
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-06-13 19:35:46 +0100

    Update User_Account.py

--- User_Account.py
+++ User_Account.py
@@ -3,12 +3,10 @@ import getpass
 import json
 from Device_Connectivity import NetworkConnection
 
-
 class UserAccount:
-
+    
     #Main function for account creation
-    def account(self):
-
+    def account(self):        
         #Opening JSON file which holds usernames and hashed/salted passwords
         try:
             with open("data.json", "r") as inFile:
@@ -21,37 +19,29 @@ class UserAccount:
         while True:
             invoke = str(input("New user or login (nu/l/x/d): "))
 
-            self.hashval = hsh.sha512()
-            self.hashvalTwo = hsh.sha512()
-
             #If new user command invoked
             if invoke == "nu":
                 #Storing new username
                 user = str(input("Enter username: "))
                 #Using getpass to obfuscate password input to interpreter
-                shv = str.encode(getpass.getpass("Enter password: "))
+                uInput = str.encode(getpass.getpass("Enter password: "))
 
                 #Salting and hashing password
-                self.hashval.update(b'salt')
-                self.hashval.update(shv)
-                hashedPassword = self.hashval.hexdigest()                
+                passOne = self.hashValues(uInput)               
 
                 #Requesting user to re-enter same password again
-                shv = str.encode(getpass.getpass("Enter password again: "))
-                self.hashvalTwo.update(b'salt')
-                self.hashvalTwo.update(shv)
-
-                x = self.hashvalTwo.hexdigest()
+                uInputTwo = str.encode(getpass.getpass("Enter password again: "))
+                passTwo = self.hashValues(uInputTwo)
 
                 #Checking if username is already taken
                 if user in self.database:
                     print("Username already exists, please use different username")
                     pass
                 #Checking if both passwords entered match
-                elif hashedPassword == x:
+                elif passOne == passTwo:
                     print("Passwords match")
                     #Adding password to dictionary assigned to username as key
-                    self.database[user] = x
+                    self.database[user] = passTwo
                     print(self.database)
 
                     #Opening and writing new username and hashed password to JSON out file
@@ -59,36 +49,25 @@ class UserAccount:
                         json.dump(self.database, outFile)
                     outFile.close()
 
-                    #Clearing hash variable
-                    self.hashval = hsh.sha512()
-
-                #Clear hash variable if passwords dont match and return to initial state   
+                #Passwords dont match return to initial state   
                 else:
                     print("Password dont match")
-                    self.hashval = hsh.sha512()
+                    
 
             #If login command invoked
-            elif invoke == "l":
-                #Clearing hash variable
-                self.hashval = hsh.sha512()
+            elif invoke == "l":                
 
                 #Requesting user input username and password and storing values to variables
                 user = str(input("Enter username: "))
-                shv = str.encode(getpass.getpass("Enter password: "))        
+                uInput = str.encode(getpass.getpass("Enter password: "))        
 
-                #Salting and hashing password
-                self.hashval.update(b'salt')
-                self.hashval.update(shv)
-                hashedPassword = self.hashval.hexdigest()
-
+                passW = self.hashValues(uInput)
                 #Authenticate username is in database and hashed and salted password value is assigned to particular username
-                if user in self.database and hashedPassword == self.database[user]:
+                if user in self.database and passW == self.database[user]:
                     print("Login successful, welcome %s" % (user))
-
                     #Getting host IP after successful user account authentication
                     self.ip = NetworkConnection.device_status()
-                    print(self.ip)
-                    
+                    print(self.ip)                    
                 else:
                     print("incorrect username/password combination")
 
@@ -99,7 +78,20 @@ class UserAccount:
             #Command used for testing purposes - prints out current dictionary of usernames and hashed passwords
             elif invoke == "d":
                 print(self.database)
+    
+    #Funtion for hashing and salting
+    def hashValues(self, value):
+        #Initialising new hash variable
+        hashval = hsh.sha512()
+        
+        #Salting and hashing password
+        hashval.update(b'salt')
+        hashval.update(value)
+        #Digesting hex hash values
+        hashedPassword = hashval.hexdigest()
 
+        return hashedPassword
+    
     def main(self):
         self.database = {}       
         self.account()
@@ -107,4 +99,3 @@ class UserAccount:
 if __name__ == "__main__":
     Account = UserAccount()
     Account.main()
-    
