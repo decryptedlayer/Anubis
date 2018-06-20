93368145b153d2885fa75b830142c404c8d18f09
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-06-20 13:30:28 +0100

    Update Local_Device.py

--- Local_Device.py
+++ Local_Device.py
@@ -19,10 +19,11 @@ class InterrogateDevice:
         try:
             #Opening socket
             netSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            #Connecting to IP 8.8.8.8 on port 80
+            #Connecting to Google Public DNS recursive name servers on port 80
             netSocket.connect(("8.8.8.8.", 80))
             #Getting local IP through socketname
             ip_address = netSocket.getsockname()[0]
+            #Closing socket
             netSocket.close()
             return ip_address
         except Exception:
