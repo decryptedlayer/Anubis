e297c3d3fbfa76a0c26cf210dbafcd5e15c5e929
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-06-20 13:31:24 +0100

    Update AES_Testing.py

--- AES_Testing.py
+++ AES_Testing.py
@@ -14,7 +14,6 @@ class AES_EAX:
         nonce, tag, ciphertext = [inFile.read(x) for x in (16, 16, -1)]
         cipher = AES.new(key, AES.MODE_EAX, nonce)
         data = cipher.decrypt_and_verify(ciphertext, tag)
-
         return data
     
     def main(self):
