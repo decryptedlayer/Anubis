268af3f18cc0826e0dc545082180048c9178520f
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-06-20 13:27:38 +0100

    Create Local_Device.py

---
+++ Local_Device.py
@@ -0,0 +1,45 @@
+import requests, socket
+from sys import platform
+
+class InterrogateDevice:
+    
+    #Function for verifying device connection status and returning local IP
+    def device_status():
+        try:
+            #Verifying IPV4 connectivity and DNS resolution
+            requests.get("http://www.msftncsi.com", timeout=3)
+            #Returning device IP address if connnection found
+            return InterrogateDevice.device_ip()
+        except requests.ConnectionError:
+            pass
+        return False
+
+    #Function for getting device IP address
+    def device_ip():
+        try:
+            #Opening socket
+            netSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            #Connecting to IP 8.8.8.8 on port 80
+            netSocket.connect(("8.8.8.8.", 80))
+            #Getting local IP through socketname
+            ip_address = netSocket.getsockname()[0]
+            netSocket.close()
+            return ip_address
+        except Exception:
+            return False
+
+    #Getting architecture of local device user running
+    def system_architecture():
+        try:
+            if platform == "linux" or platform == "linux2":
+                return "Linux"
+            elif platform == "darwin":
+                return "OS X"
+            elif platform == "win32":
+                return "Windows"
+            else:
+                raise Exception
+        except Exception:
+            pass
+        return False
+    
