578b208b04e72175c47b0a0b91493c4ec09c0e18
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-06-20 22:17:24 +0100

    Update Local_Device.py

--- Local_Device.py
+++ Local_Device.py
@@ -8,10 +8,13 @@ class InterrogateDevice:
         try:
             #Verifying IPV4 connectivity and DNS resolution
             requests.get("http://www.msftncsi.com", timeout=3)
+            
             #Returning device IP address if connnection found
             return InterrogateDevice.device_ip()
+        
         except requests.ConnectionError:
             pass
+        
         return False
 
     #Function for getting device IP address
@@ -19,13 +22,18 @@ class InterrogateDevice:
         try:
             #Opening socket
             netSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            
             #Connecting to Google Public DNS recursive name servers on port 80
             netSocket.connect(("8.8.8.8.", 80))
+            
             #Getting local IP through socketname
             ip_address = netSocket.getsockname()[0]
+            
             #Closing socket
             netSocket.close()
+            
             return ip_address
+        
         except Exception:
             return False
 
@@ -34,12 +42,16 @@ class InterrogateDevice:
         try:
             if platform == "linux" or platform == "linux2":
                 return "Linux"
+            
             elif platform == "darwin":
                 return "OS X"
+            
             elif platform == "win32":
                 return "Windows"
+            
             else:
                 raise Exception
+                
         except Exception:
             pass
         return False
