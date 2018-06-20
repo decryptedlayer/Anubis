a19cb35dc3d61d078231cab0066485253a8cc9b9
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-06-20 13:04:59 +0100

    Update Device_Connectivity.py

--- Device_Connectivity.py
+++ Device_Connectivity.py
@@ -8,8 +8,22 @@ class NetworkConnection:
             #Verifying IPV4 connectivity and DNS resolution
             requests.get("http://www.msftncsi.com", timeout=3)
             #Returning device IP address if connnection found
-            return socket.gethostbyname(socket.gethostname())
+            return NetworkConnection.device_ip()
         except requests.ConnectionError:
             pass
         return False
 
+    #Function for getting device IP address
+    def device_ip():
+        try:
+            #Opening socket
+            netSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            #Connecting to IP 8.8.8.8 on port 80
+            netSocket.connect(("8.8.8.8.", 80))
+            #Getting local IP through socketname
+            ip_address = netSocket.getsockname()[0]
+            netSocket.close()
+            return ip_address
+        except Exception:
+            return False
+    
