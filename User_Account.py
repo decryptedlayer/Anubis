4aaa04d9ee75f564b033ceb10a878c6a61bebd4d
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-06-20 13:16:20 +0100

    Update User_Account.py

--- User_Account.py
+++ User_Account.py
@@ -5,8 +5,7 @@ from Detect_Device import DeviceArchitecture
 class UserAccount:
     
     #Main function for account creation
-    def genAccount(self):
-        
+    def genAccount(self):        
         #Opening JSON file which holds usernames, hashed passwords and associated salts
         try:
             with open("data.json", "r") as inFile:
@@ -14,30 +13,24 @@ class UserAccount:
             inFile.close()
         except:
             pass
-
         #Code block invoking login or account creation
         while True:
             invoke = str(input("Log-in or create a new account (l/nu/x/d): "))
-
             #If new user command invoked
             if invoke.lower() == "nu":
                 #Storing new username
                 user = str(input("Enter username: "))
                 #Using getpass to obfuscate password input to interpreter
                 uInput = str.encode(getpass.getpass("Enter password: "))
-
                 #Generating new random salt value
-                slt = self.saltValue()
-                
+                slt = self.saltValue()                
                 #Salting and hashing password
-                passOne = self.hashValues(uInput, slt)               
-
+                passOne = self.hashValues(uInput, slt)
                 #Requesting user to re-enter same password again
                 uInputTwo = str.encode(getpass.getpass("Enter password again: "))
-
                 #Hashing and salting second password input
                 passTwo = self.hashValues(uInputTwo, slt)
-
+                
                 #Checking if username is already taken
                 if user in self.database:
                     print("Username already exists, please use different username")
@@ -48,26 +41,21 @@ class UserAccount:
                     print("Passwords match")
                     #Adding hashed password and salt to dictionary assigned to username as key
                     self.database[user]= [passTwo, slt.decode('ascii'), "NetworkConnection.device_status()"]
-
                     #Printing database for testing purposes
                     "print(self.database)"
-
                     #Opening and writing new username and hashed password to JSON out file
                     with open("data.json", "w") as outFile:
                         json.dump(self.database, outFile)
                     outFile.close()
-
                 #Passwords dont match return to initial state   
                 else:
-                    print("Passwords dont match")                    
+                    print("Passwords dont match")           
 
             #If login command invoked
-            elif invoke.lower() == "l":                
-
+            elif invoke.lower() == "l":
                 #Requesting user input username and password and storing values to variables
                 user = str(input("Enter username: "))
-                uInput = str.encode(getpass.getpass("Enter password: "))
-                
+                uInput = str.encode(getpass.getpass("Enter password: "))                
                 #Authenticate username is in database and hashed and salted password value is assigned to particular username
                 if user in self.database:
                     #Extracting original salt associated with username
@@ -76,7 +64,6 @@ class UserAccount:
                     uSlt = str.encode(uSlt)
                     #Hashing and salting password for user to login
                     passW = self.hashValues(uInput, uSlt)
-
                     #Verifying hashed and salted password input same as hashed salted password in database
                     if passW == self.database[user][0]:
                         print("-----------------\nLogin successful!")                        
@@ -106,10 +93,8 @@ class UserAccount:
     def hashValues(self, value, salt):        
         #Encoding salt from string to binary
         hashVal = bcrypt.kdf(value, salt, 64, 100)
-
         #Generating hex value from binary hash
-        key = binascii.hexlify(hashVal)
-        
+        key = binascii.hexlify(hashVal)        
         #Returning decoded hex value
         return key.decode('ascii')
 
@@ -117,7 +102,6 @@ class UserAccount:
     def saltValue(self):
         #Utilising bcrypt to generate random salt values
         slt = bcrypt.gensalt(20)
-
         return slt
 
     def userAccount(self, user):
