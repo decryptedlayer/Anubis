170b575b829fa257fde5ceb24cf8ea1dc490fb77
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-07-06 09:34:36 +0100

    Update AES_Testing.py

--- AES_Testing.py
+++ AES_Testing.py
@@ -1,28 +1,35 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
-class AES_EAX:
+def encrypt(p, key, file):
+    cipher = AES.new(key, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(p)
+    file_out = open(file, "wb")
+    [file_out.write(x) for x in (cipher.nonce, tag, ciphertext)]
 
-    def encrypt(self, p, key):
-        cipher = AES.new(key, AES.MODE_EAX)
-        ciphertext, tag = cipher.encrypt_and_digest(p)
-        file_out = open("encrypted.bin", "wb")
-        [file_out.write(x) for x in (cipher.nonce, tag, ciphertext)]
-
-    def decrypt(self, key):
-        inFile = open("encrypted.bin", "rb")
-        nonce, tag, ciphertext = [inFile.read(x) for x in (16, 16, -1)]
-        cipher = AES.new(key, AES.MODE_EAX, nonce)
-        data = cipher.decrypt_and_verify(ciphertext, tag)
-        return data
+def readCiphertext(file):
+    inFile = open(file, "rb")
+    nonce, tag, ciphertext = [inFile.read(x) for x in (16, 16, -1)]
+    return nonce,
     
-    def main(self):
-        key = get_random_bytes(32)
-        plaintext = b"test"
-        self.encrypt(plaintext, key)
-        print(self.decrypt(key))
-        
+def decrypt(key, file):
+    inFile = open(file, "rb")
+    nonce, tag, ciphertext = [inFile.read(x) for x in (16, 16, -1)]
+    cipher = AES.new(key, AES.MODE_EAX, nonce)
+    data = cipher.decrypt_and_verify(ciphertext, tag)
 
-if __name__ == "__main__":
-    AESEAX = AES_EAX()
-    AESEAX.main()
+    return data
+    
+def encryptionTesting(plaintext):
+    key = get_random_bytes(32)
+    #plaintext = b"test"
+    file = "misc.bin"
+    try:
+        readCiphertext(file)
+        encrypt(plaintext, key, file)
+        decrypt(key, file)
+    except:
+        pass
+    
+    return readCiphertext(file)
+
