d243254cbc51d99de4c6639cfac5f76dd12ba4e6
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-08-06 14:06:53 +0100

    Update AES_EAX.py

--- Encryption/AES_EAX.py
+++ Encryption/AES_EAX.py
@@ -4,7 +4,12 @@ from Crypto.Random import get_random_bytes
 class AES_EAX:
     def __init__(self):        
         self.data = b"hello"
-        self.key = get_random_bytes(16)
+        
+        #32 random byted for 256-bit encryption key
+        self.key = get_random_bytes(32)
+
+        #
+        self.AES_Mode = AES.MODE_GCM
 
     def writeKey(self, key):
         file_out = open("key.bin", "wb")
@@ -13,38 +18,47 @@ class AES_EAX:
 
 
     def importKey(self):
-        sessionKey = str()
+        sessionKey = b""
         file_in = open("key.bin", "rb")
         for i in file_in:
             sessionKey = i
         return(sessionKey)
 
-    def encryption(self):       
-        cipher = AES.new(self.key, AES.MODE_EAX)
+    def encryption(self):
+        #Generating new cipher and initialising block cipher mode of operation
+        cipher = AES.new(self.key, AES.MODE_GCM)
         ciphertext, tag = cipher.encrypt_and_digest(self.data)
 
         file_out = open("encrypted.bin", "wb")
         [file_out.write(x) for x in (cipher.nonce, tag, ciphertext)]
         file_out.close()
 
+        #Writing out current session key
         self.writeKey(self.key)
 
         return self.key
 
     def decryption(self):        
         file_in = open("encrypted.bin", "rb")
-        nonce, tag, ciphertext = [ file_in.read(x) for x in (16, 16, -1) ]
+        nonce, tag, ciphertext = [file_in.read(x) for x in (16, 16, -1)]
         file_in.close()
 
-        cipher = AES.new(self.key, AES.MODE_EAX, nonce)
+        cipher = AES.new(self.key, AES.MODE_GCM, nonce)
         data = cipher.decrypt_and_verify(ciphertext, tag)
 
-        return data
+        return data.decode("utf-8")
+
+    def keyIntegrityCheck(self):
+        if self.importKey() == self.key:
+            return True
+        else:
+            return False
 
     def main(self):
         print(self.encryption())
         print(self.decryption())
         #print(self.importKey())
+        print(self.keyIntegrityCheck())
 
 if __name__ == "__main__":
     AESEncryption = AES_EAX()
