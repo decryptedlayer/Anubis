c1b9ed3d7bc1c6f9e114304087a6a30dadb7e7f8
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-12-18 20:13:17 +0000

    Update Salsa20Algorithm.py

--- Encryption/Salsa20/Salsa20Algorithm.py
+++ Encryption/Salsa20/Salsa20Algorithm.py
@@ -49,7 +49,9 @@ class Salsa:
 
     #Salsa20 Encryption function
     def encrypt(self, c, p, k):
+        #Generating new cipher based on key
         cipher = Salsa20.new(k)
+        #Encrypting cipher
         ciphertext = c.encrypt(p)
 
         #Returning ciphertext and nonce used for decryption
@@ -57,21 +59,26 @@ class Salsa:
 
     #Salsa20 Decryption function
     def decrypt(self, c, n, k):
+        #Generating new cipher based on key and nonce
         cipher = Salsa20.new(k, n)
+
+        #Decrypting cipher
         plaintext = cipher.decrypt(c)
 
-        return plaintext
+        return plaintext.decode('utf-8')
 
     def main(self):
+        #Defining local variables based on constructors
         cipher = self.cipher
         plaintext = self.plaintext
         key = self.key
-        
-        ciphertext = self.encrypt(cipher, plaintext, key)
-        plain = self.decrypt(ciphertext[0], ciphertext[1], key)
 
-        print(ciphertext[:])
-        print(plain)
+        #Initialising functions for encryption and decryption
+        encryption = self.encrypt(cipher, plaintext, key)
+        decryption = self.decrypt(encryption[0], encryption[1], key)
+
+        print(encryption[:])
+        print(decryption)
 
 if __name__ == "__main__":
     Sal20 = Salsa()
