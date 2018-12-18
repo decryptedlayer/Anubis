d4c35b34e8b3a97c6f5f6e8ab71d9fd9cc2c7e28
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-12-18 19:43:49 +0000

    Update Salsa20Algorithm.py

--- Encryption/Salsa20/Salsa20Algorithm.py
+++ Encryption/Salsa20/Salsa20Algorithm.py
@@ -1,13 +1,48 @@
+import bcrypt, binascii
 from Crypto.Cipher import Salsa20
+
+"""
+Example of using Salsa20 as a private key messaging protocol
+where both parties use a known hashed password in order to
+to encrypt and decrypt messages.
+"""
 
 class Salsa:
 
     def __init__(self):
-        #Constructing key of 16 bits
-        self.key = b'0123456789012345'
+        #Constructing secret key of 16 bits from hashed password
+        self.key = self.password()
+
+        #Checking function properties
+        print(self.check(self.key))
+
+        #Generating new Salsa20 Cipher
         self.cipher = Salsa20.new(self.key)
+
+        #Defining example plaintext
         self.plaintext = b"hello"
 
+    def hash(self, p):
+        #Defining example salt value
+        salt = b"1234"
+        
+        hv = bcrypt.kdf(p, salt, 64, 100)
+        cipher = binascii.hexlify(hv)
+
+        return cipher
+
+    def check(self, x):
+        l, typ = len(x), type(x)
+
+        return l, typ
+        
+
+    def password(self):
+        password = str(input("Enter Password of 16 characters: ")).encode()
+        hashValue = self.hash(password)
+
+        return hashValue[:16]
+    
     def encrypt(self, c, p):
         ciphertext = c.encrypt(p)
 
